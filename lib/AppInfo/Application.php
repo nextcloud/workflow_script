00a2ac4a3065209b4c50e1f4fd6217f793195108
Julius Härtl <jus@bitgrid.net>
2019-10-10 11:39:22 +0200

    Move to new Operation registration
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -23,6 +23,10 @@
 
 namespace OCA\WorkflowScript\AppInfo;
 
+use OCA\WorkflowScript\Operation;
+use OCP\WorkflowEngine\IManager;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
 class Application extends \OCP\AppFramework\App {
 
 	/**
@@ -30,6 +34,11 @@ class Application extends \OCP\AppFramework\App {
 	 */
 	public function __construct() {
 		parent::__construct('workflow_script');
+		\OC::$server->getEventDispatcher()->addListener(IManager::EVENT_NAME_REG_OPERATION, function (GenericEvent $event) {
+			$operation = \OC::$server->query(Operation::class);
+			$event->getSubject()->registerOperation($operation);
+			\OC_Util::addScript('workflow_script', 'admin');
+		});
 	}
 
 }
