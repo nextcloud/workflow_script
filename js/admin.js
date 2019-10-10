4801741c36d70b11be342c6e2793cc72e3ca6399
Julius Härtl <jus@bitgrid.net>
2019-10-10 14:57:27 +0200

    Keep the variable private
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/admin.js
+++ js/admin.js
@@ -17,55 +17,60 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-var Component = {
-	name: 'WorkflowScript',
-	render: function (createElement) {
-		var self = this
-		return createElement('div', {
-			style: {
-				width: '100%'
-			},
-		}, [
-			createElement('input', {
-				attrs: {
-					type: 'text'
-				},
-				domProps: {
-					value: self.value,
-					required: 'true'
-				},
+
+(function() {
+
+	var Component = {
+		name: 'WorkflowScript',
+		render: function (createElement) {
+			var self = this
+			return createElement('div', {
 				style: {
 					width: '100%'
 				},
-				on: {
-					input: function (event) {
-						self.$emit('input', event.target.value)
+			}, [
+				createElement('input', {
+					attrs: {
+						type: 'text'
+					},
+					domProps: {
+						value: self.value,
+						required: 'true'
+					},
+					style: {
+						width: '100%'
+					},
+					on: {
+						input: function (event) {
+							self.$emit('input', event.target.value)
+						}
 					}
-				}
-			}),
-			createElement('a', {
-				attrs: {
-					href: self.link
-				},
-				style: {
-					color: 'var(--color-text-maxcontrast)'
-				}
-			}, self.description)
-		])
-	},
-	props: {
-		value: ''
-	},
-	data: function () {
-		return {
-			description: t('workflow_script', 'Available placeholder variables are listed in the documentation') + '↗',
-			link: 'https://github.com/nextcloud/workflow_script#placeholders'
+				}),
+				createElement('a', {
+					attrs: {
+						href: self.link
+					},
+					style: {
+						color: 'var(--color-text-maxcontrast)'
+					}
+				}, self.description)
+			])
+		},
+		props: {
+			value: ''
+		},
+		data: function () {
+			return {
+				description: t('workflow_script', 'Available placeholder variables are listed in the documentation') + '↗',
+				link: 'https://github.com/nextcloud/workflow_script#placeholders'
+			}
 		}
-	}
-};
+	};
 
-OCA.WorkflowEngine.registerOperator({
-	id: 'OCA\\WorkflowScript\\Operation',
-	operation: '',
-	options: Component
-});
+	OCA.WorkflowEngine.registerOperator({
+		id: 'OCA\\WorkflowScript\\Operation',
+		operation: '',
+		options: Component
+	});
+
+})();
